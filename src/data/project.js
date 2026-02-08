2e59a83463d46d8bf6774f6c0d24091114cee3c7
Esther Quirós <estherbequiros@gmail.com>
2026-02-08 14:17:21 +0100

    update project card styles

--- src/data/project.js
+++ src/data/project.js
@@ -7,4 +7,20 @@ export const projects = [
     projectUrl:
       "https://estherquiros.github.io/modulo-2-evaluacion-intermedia-estherquiros/",
   },
+  {
+    id: 2,
+    title: "Buscador de películas",
+    techs: ["JavaScript", "CSS"],
+    imageUrl: "https://via.placeholder.com/300x200",
+    projectUrl:
+      "https://estherquiros.github.io/modulo-2-evaluacion-final-estherquiros/",
+  },
+  {
+    id: 3,
+    title: "Buscador de personajes de Rick y Morty",
+    techs: ["React", "CSS"],
+    imageUrl: "https://via.placeholder.com/300x200",
+    projectUrl:
+      "https://estherquiros.github.io/modulo-3-evaluacion-final-estherquiros/",
+  },
 ];
