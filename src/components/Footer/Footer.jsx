7a2163b4e38c9b76928e66878d6bf398990aedbf
Esther Quirós <estherbequiros@gmail.com>
2026-02-11 17:34:57 +0100

    add footer styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,9 @@
 import "./Footer.scss";
+import {
+  PiGithubLogoLight,
+  PiSmileyLight,
+  PiLinkedinLogoLight,
+} from "react-icons/pi";
 export const Footer = () => {
   return (
     <footer className="footer">
@@ -25,7 +30,51 @@ export const Footer = () => {
       <div className="footer__title">
         <h2 className="footer__title-text font-title">Esther's Portfolio</h2>
       </div>
-      <div className="footer__bottom"></div>
+      <div className="footer__bottom">
+        <div className="footer__social">
+          <ul className="footer__links">
+            <li>
+              <a className="link" href="mailto:estherbequiros@gmail.com">
+                <PiSmileyLight size={30} />
+              </a>
+            </li>
+            <li>
+              <a
+                className="link"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/estherquiros"
+              >
+                <PiGithubLogoLight size={30} />
+              </a>
+            </li>
+            <li>
+              <a
+                className="link"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.linkedin.com/in/esther-quir%C3%B3s/"
+              >
+                <PiLinkedinLogoLight size={30} />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer__copyright">
+        <p>Hecho con mezcla de azúcar, especias y muchas cosas bonitas 🦄</p>
+        <p>
+          Inspirado en{" "}
+          <a
+            className="footer__copyright-link link"
+            href="https://www.heygirlstudio.co.uk/"
+            target="__blank"
+            rel="noopener noreferrer"
+          >
+            Hey Girl Studio
+          </a>
+        </p>
+      </div>
     </footer>
   );
 };
