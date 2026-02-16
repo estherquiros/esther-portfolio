03712c177341d85b883188ffd8555d4943812d44
Esther Quirós <estherbequiros@gmail.com>
2026-02-16 14:26:09 +0100

    order project

--- src/data/project.js
+++ src/data/project.js
@@ -7,23 +7,16 @@ export const projects = [
     projectUrl:
       "https://estherquiros.github.io/modulo-2-evaluacion-intermedia-estherquiros/",
   },
+
   {
     id: 2,
-    title: "Online Store",
-    techs: ["JavaScript", "CSS"],
-    imageUrl: "https://via.placeholder.com/300x200",
-    projectUrl:
-      "https://beta.adalab.es/modulo-2-evaluacion-final-estherquiros/",
-  },
-  {
-    id: 3,
     title: "Filtro de Animal Crossing",
     techs: ["React", "CSS"],
     imageUrl: "https://via.placeholder.com/300x200",
     projectUrl: "https://estherquiros.github.io/Filtro-Animal-Crossing/",
   },
   {
-    id: 4,
+    id: 3,
     title: "Encuentra las parejas",
     techs: ["React", "CSS"],
     imageUrl: "https://via.placeholder.com/300x200",
@@ -36,5 +29,13 @@ export const projects = [
     imageUrl: "https://via.placeholder.com/300x200",
     projectUrl:
       "https://beta.adalab.es/modulo-3-evaluacion-final-estherquiros/",
+  },
+  {
+    id: 5,
+    title: "Online Store",
+    techs: ["JavaScript", "CSS"],
+    imageUrl: "https://via.placeholder.com/300x200",
+    projectUrl:
+      "https://beta.adalab.es/modulo-2-evaluacion-final-estherquiros/",
   },
 ];
